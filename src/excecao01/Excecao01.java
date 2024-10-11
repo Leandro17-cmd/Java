2cc104b2e01827aa6d251b003e09e48ad176e082
Leandro17-cmd <182236799+Leandro17-cmd@users.noreply.github.com>
2024-10-11 13:55:01 +0100

    Meu primeiro commit no Netbeans

---
+++ src/excecao01/Excecao01.java
@@ -0,0 +1,16 @@
+package excecao01;
+
+/**
+ *
+ * @author al.leandroandrade
+ */
+public class Excecao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
